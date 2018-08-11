718ecbdafd83d3c977c0bbd0f1f70bbe432b4fb7
nick paridon <paridonn1@gmail.com>
2018-08-11 10:23:02 -0400

    update to public folder

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path')
 
 var app = express()
 var PORT = process.env.PORT || 3000
@@ -6,17 +7,21 @@ var PORT = process.env.PORT || 3000
 var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 
+app.use(express.static('/public'));
+app.use(express.static(path.join(__dirname, '/public')));
+
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'handlebars');
-app.set('views', './views')
+app.set('views', './views');
 
 var burgersController = require('./controllers/burgersController');
 
 app.use('/', burgersController);
-app.use(express.static('public'))
+
 
 
 
